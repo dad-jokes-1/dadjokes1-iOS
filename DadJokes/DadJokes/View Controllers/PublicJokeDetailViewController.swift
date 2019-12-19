fa64042980b2319a714658d75b8478b172d793e0
John Kouris <jdkouris@gmail.com>
2019-12-18 20:20:10 -0800

    Added the basic layout and view controller files

---
+++ DadJokes/DadJokes/View Controllers/PublicJokeDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PublicJokeDetailViewController.swift
+//  DadJokes
+//
+//  Created by John Kouris on 12/18/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class PublicJokeDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
