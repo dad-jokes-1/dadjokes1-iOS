376539df38f4eaae612a96679470c999c64da321
John Kouris <jdkouris@gmail.com>
2019-12-19 19:49:48 -0800

    Updated the corner radius to be more round

--- DadJokes/DadJokes/View Controllers/SignInViewController.swift
+++ DadJokes/DadJokes/View Controllers/SignInViewController.swift
@@ -18,7 +18,7 @@ class SignInViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupViews()
     }
     
     func setupViews() {
