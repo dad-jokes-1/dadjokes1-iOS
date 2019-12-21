532eb617b9e2d874456745df69207b0a790565d9
Christian Lorenzo <clbmiami2004@gmail.com>
2019-12-21 10:37:20 -0800

    Added CoreData model and created a Joke Representation Document.

---
+++ DadJokes/DadJokes/Models/JokeRepresentation.swift
@@ -0,0 +1,22 @@
+//
+//  JokeRepresentation.swift
+//  DadJokes
+//
+//  Created by Lambda_School_Loaner_201 on 12/21/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import Foundation
+
+struct JokeRepresentation: Equatable, Codable {
+    
+    let id: UUID
+    let question: String
+    let answer: String
+    let username: String
+}
+
+struct JokeRepresentations: Codable {
+    
+    let results: [JokeRepresentation]
+}
